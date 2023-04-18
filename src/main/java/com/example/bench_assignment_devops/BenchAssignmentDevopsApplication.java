f7052d30e986d1bd7a83a1779e53c44254f9b109
simrangawri <42602088+simrangawri@users.noreply.github.com>
2023-04-18 18:53:02 +0530

    Add files via upload

---
+++ src/main/java/com/example/bench_assignment_devops/BenchAssignmentDevopsApplication.java
@@ -0,0 +1,13 @@
+package com.example.bench_assignment_devops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BenchAssignmentDevopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BenchAssignmentDevopsApplication.class, args);
+	}
+
+}
